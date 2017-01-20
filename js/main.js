3a6ef6f7cf4d1aa11cdcb8590bb49b2a60fc7356
Tatiana Limache Quispe <tati34che@gmail.com>
2017-01-20 07:58:02 -0500

    Proyecto a un 80%

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
+//icoId.addEventListener("click", eliminar, false); 
+
 function agregar(container,tarea)
 {
+	//Creacion de un Nodo div con clase checkbox
 	var div=document.createElement('div');
-	div.id="id";
+	div.id="idDiv";
 	div.className="checkbox , list";
+
+	//Creacion de un nodo iono
+	var icono=document.createElement('i');
+	icono.className="fa fa-trash-o ";
+	icono.id="idIco";
+	
+	//Creacion de un Nodo label
 	var label=document.createElement('label');
+	label.id="idLbl"
+	//Creacion de un Nodo checkbox
 	var checkbox=document.createElement('input');
 	checkbox.type = "checkbox";
+	checkbox.id="idchk";
+
+	//Insercion de los Nodos 
 	div.appendChild(label);
 	label.appendChild(checkbox);
 	label.appendChild(document.createTextNode(tarea));
+	label.appendChild(icono);
 	container.appendChild(div);	
+	//eventos addlistener
+	//chkId.addEventListener('click', eliminar(), false); 
 }
 function eliminar()
 {
-  var nodos=document.getElementById('id');
+  var nodos=document.getElementById('idDiv');
   nodos.parentNode.removeChild(nodos);
 }
 function añadirTareas()
@@ -23,3 +41,11 @@ function añadirTareas()
 		agregar(listaTareas,tarea);
 	}
 }
+
+/*
+if (idchk.checked==true) 
+{
+	idLbl.setAttribute("style","text-decoration:line-through;");
+}
+//
+*/
