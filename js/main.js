0d88d1faff43519b4919208e409cedeb3ae7704b
Tatiana Limache Quispe <tati34che@gmail.com>
2017-01-20 05:26:20 -0500

    Añadi funcion agregar

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function agregar(container,tarea)
+{
+	var div=document.createElement('div');
+	var label=document.createElement('label');
+	var checkbox=document.createElement('input');
+	checkbox.type = "checkbox";
+	div.appendChild(label);
+	label.appendChild(checkbox);
+	label.appendChild(document.createTextNode(tarea));
+	container.appendChild(div);
+	
+}
+function añadirTareas()
+{
+	var tarea = document.getElementById("tarea").value;
+	if(tarea !=""){
+		agregar(listaTareas,tarea);
+	}
+}
